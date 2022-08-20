2c75216b1420e9cf6239b9cabfbfad19e7f7e44e
WarzeEgg <contact@warze.org>
2022-08-20 11:05:00 +0200

    New pages

--- app.py
+++ app.py
@@ -2,33 +2,10 @@
 from flask import Flask, render_template
 import os
 import numpy as np
+from products import products
 
 # This is the port number, defaults to 5000 if no environment variable is found
 port = int(os.getenv('PORT', 5000))
-
-products = {
-    'rimowa': [
-        ['Rimowa Essential', 'bagblack', '2,000'],
-        ['Strong Travels', 'black', '1,800'],
-        ['Blueness', 'bluebag', '2,400'],
-        ['Sage Serrano', 'green', '1,200'],
-        ['Pastel Yellow', 'suitcase', '1,700'],
-        ['Pretty Pink', 'pink', '3,000'],
-        ['Round Wrap', 'round', '1,000'],
-        ['Bright Days', 'yellow', '4,110'],
-        ['Simple Side', 'medium', '1,700']
-    ],
-    'globetrotter': [
-        ['Boss Black', 'blackcase', '2,000'],
-        ['Bold peach', 'fantastic', '5,800'],
-        ['Classic English', 'browncase', '2,400'],
-        ['Woven Women', 'brownpurse', '1,200'],
-        ['Hard-cover Hold', 'handback', '8,100'],
-        ['Sheepish', 'sheep', '3,400'],
-        ['I Have Some Baggage', 'suitcase', '5,000'],
-        ['Turtle Takes', 'turtle', '3,700']
-    ]
-}
 
 # Creating the app, it will be referenced in all of the routes and when starting the server
 app = Flask(__name__)
@@ -42,6 +19,21 @@ def homepage():
 @app.route('/catalogue')
 def catalogue():
     return render_template('catalogue.html')
+
+# The contact page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# The about page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# The login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 # The product listings
 @app.route('/products/<category>')
---
+++ products.py
@@ -0,0 +1,41 @@
+products = {
+    'rimowa': [
+        ['Rimowa Essential', 'bagblack', '2,000'],
+        ['Strong Travels', 'black', '1,800'],
+        ['Blueness', 'bluebag', '2,400'],
+        ['Sage Serrano', 'green', '1,200'],
+        ['Pastel Yellow', 'suitcase', '1,700'],
+        ['Pretty Pink', 'pink', '3,000'],
+        ['Round Wrap', 'round', '1,000'],
+        ['Bright Days', 'yellow', '4,110'],
+        ['Simple Side', 'medium', '1,700']
+    ],
+    'globetrotter': [
+        ['Boss Black', 'blackcase', '2,000'],
+        ['Bold peach', 'fantastic', '5,800'],
+        ['Classic English', 'browncase', '2,400'],
+        ['Woven Women', 'brownpurse', '1,200'],
+        ['Hard-cover Hold', 'handback', '8,100'],
+        ['Sheepish', 'sheep', '3,400'],
+        ['I Have Some Baggage', 'suitcase', '5,000'],
+        ['Turtle Takes', 'turtle', '3,700']
+    ],
+    'montblanc': [
+        ['Classic', 'backpack', '1,300'],
+        ['Clutch', 'black', '830'],
+        ['The Grate', 'heavysuitcase', '830'],
+        ['Creative Case', 'suitcasedoodle', '3,600']
+    ],
+    'mulberry': [
+        ['Blue Base', '1', '840'],
+        ['Adventure Time', 'bag', '1,040'],
+        ['Regular Life', 'bag1', '890'],
+        ['Hold Me', 'beige', '950'],
+        ['White Wants', 'blocks', '900'],
+        ['Brownie Batter', 'brownbag', '1,100'],
+        ['Checking In', 'checks', '350'],
+        ['Yellow Yolk', 'yellow', '1,500'],
+        ['Green Grass', 'green', '600'],
+        ['Boho', 'orange', '1,300']
+    ]
+}
