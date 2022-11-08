efbcd8a597d36cd0818b93ce845907babbe7b506
WarzeEgg <contact@warze.org>
2022-11-08 12:27:49 +0100

    Added .j2 everywhere

--- app.py
+++ app.py
@@ -12,42 +12,42 @@ app = Flask(__name__)
 # The homepage
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('index.html.j2')
 
 # The catalogue
 @app.route('/catalogue')
 def catalogue():
-    return render_template('catalogue.html')
+    return render_template('catalogue.html.j2')
 
 # The contact page
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html.j2')
 
 # The about page
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html.j2')
 
 # The login page
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html.j2')
 
 # The product listings
 @app.route('/products/<category>')
 def productlistings(category):
     if category.lower() not in products:
-        return render_template('404.html')
+        return render_template('404.html.j2')
     
     selected_category = products[category.lower()]
-    return render_template('products.html', products = selected_category, section = category.capitalize())
+    return render_template('products.html.j2', products = selected_category, section = category.capitalize())
 
 # A single product
 @app.route('/products/<category>/<product>')
 def singleproduct(category, product):
     if category.lower() not in products:
-        return render_template('404.html')
+        return render_template('404.html.j2')
     
     selected_product = None
 
@@ -57,15 +57,15 @@ def singleproduct(category, product):
             selected_product = item
     
     if selected_product == None:
-        return render_template('404.html')
+        return render_template('404.html.j2')
 
-    return render_template('singleproduct.html', category = category, product = selected_product, section = category.capitalize())
+    return render_template('singleproduct.html.j2', category = category, product = selected_product, section = category.capitalize())
 
 # The 404 page
 @app.errorhandler(404)
 def errorhandler(e):
     print(e)
-    return render_template('404.html')
+    return render_template('404.html.j2')
 
 # Start hosting the server
 app.run(host='0.0.0.0', port=port, debug=True)
